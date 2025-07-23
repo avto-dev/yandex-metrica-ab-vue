f4ba86ba222ac8ae0bc27b66137c53485cf9cf20
Eugene Shangin <fessracha@gmail.com>
2025-07-23 16:03:23 +0500

    fix export use-experiment

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './provider/varioqub-ab-tests-provider';
+export * from './use-experiment/use-experiment'
 export { default as VarioqubAntiflicker } from './antiflicker/varioqub-antiflicker';
